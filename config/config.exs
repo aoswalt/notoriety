60ce7b5d4116de71647195e47635fc9ad7e84436
Adam Oswalt <email@adamoswalt.com>
2020-09-07 19:44:25 -0500

    Switch source based on env

--- config/config.exs
+++ config/config.exs
@@ -4,8 +4,15 @@ case Mix.env() do
   :dev ->
     config :mix_test_watch,
       clear: true
+
   _ ->
     nil
 end
 
-config :notoriety, :source, Notoriety.Source.InMemory
+case Mix.env() do
+  :prod ->
+    config :notoriety, :source, Notoriety.Source.FS
+
+  _ ->
+    config :notoriety, :source, Notoriety.Source.InMemory
+end
