4e15e3f2a495a2fde6f45978e1c0c8c30728324a
Adam Oswalt <email@adamoswalt.com>
2020-09-07 19:37:56 -0500

    Add basic implementation of file system source

--- lib/notoriety/source/fs.ex
+++ lib/notoriety/source/fs.ex
@@ -1,3 +1,13 @@
-# defmodule Notoriety.Source.FS do
-#   @behaviour Notoriety.Source
-# end
+defmodule Notoriety.Source.FS do
+  @behaviour Notoriety.Source
+
+  def list_files(pattern \\ "notes/**/*.md") do
+    pattern
+    |> Path.wildcard()
+    |> Enum.map(&{&1, File.read!(&1)})
+  end
+
+  def save_index(index, filename \\ "index.md") do
+    File.write(filename, index)
+  end
+end
